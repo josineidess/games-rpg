2ff5063103585ea63de63d14a20ed2cfdec734cf
josineidess <josineide1402@gmail.com>
2021-01-13 20:52:14 -0300

    sala do tempo

---
+++ sala_do_tempo.py
@@ -0,0 +1,16 @@
+import pyperclip
+ 
+palavra = input("palavra: ")
+palavra = palavra.upper()
+quant = int(input('vezes: '))
+
+variavel = ''
+
+while(quant != 0):
+  variavel+=palavra
+  variavel+='\n'
+  quant-=1
+
+print(variavel)
+pyperclip.copy(variavel)
+
